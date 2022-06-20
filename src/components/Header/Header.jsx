fc027933fce6401454ef8e824a275e44e9589608
Armando Almanza <armando.s.almanza@gmail.com>
2022-06-20 16:12:46 -0500

    bug fixing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,21 @@ const Header = () => {
     return (
         <header className="text-center p-4 flex justify-around items-center flex-col md:flex-row">
             <div>
-            <h3 className="text-5xl">
-                Wiki<span className="text-violet-400">Cat</span> 🐈‍⬛
-            </h3>
-        </div>
+                <h3 className="text-5xl">
+                    Wiki<span className="text-violet-400">Cat</span> 🐈‍⬛
+                </h3>
+            </div>
             <nav className="flex justify-center mt-2 gap-7">
-                <Link className="mr-3 text-2xl hover:text-violet-600 transition-all" to="/">
+                <Link
+                    className="mr-3 text-2xl hover:text-violet-600 transition-all"
+                    to="/"
+                >
                     Home
                 </Link>
-                <Link className="text-2xl hover:text-violet-600 transition-all" to="/cats">
+                <Link
+                    className="text-2xl hover:text-violet-600 transition-all"
+                    to="/cats"
+                >
                     Cats
                 </Link>
             </nav>
